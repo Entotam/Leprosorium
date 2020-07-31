4c5dd0f40268941c8bbfd9167b2f05761df32d82
Sergei Chernov <dozzator@gmail.com>
2020-08-01 01:52:21 +0400

    add configure and db

--- app.rb
+++ app.rb
@@ -4,6 +4,26 @@ require 'sinatra/reloader'
 require 'pony'
 require 'sqlite3'
 
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+	return @db	
+end
+
+before do
+	init_db
+end
+
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS "Posts" (
+		"id"	INTEGER,
+		"created_data"	REAL,
+		"content"	TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	)'
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
